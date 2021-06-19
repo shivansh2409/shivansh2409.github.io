821dfcd0466abfb3d73713fd242d8a8ee49bc976
Shivansh <shivansh.ajay.mishra@gmail.com>
2021-06-20 03:03:19 +0530

    footer
    foot

--- forms/contact.php
+++ forms/contact.php
@@ -24,14 +24,13 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
+    'host' => 'gmail.com',
+    'username' => 'unnated27@gmail.com',
+    'password' => 'iimbangalore',
+    'port' => '465'
   );
-  */
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
